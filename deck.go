143d4006e90fb9c43e106ea007664da8baf65cd3
Leo Rubiano <leofofeo@Leos-MacBook-Pro.local>
2019-07-28 20:56:01 -0500

    refactor random card seletion to dealRandom, create simple deal function with one hand size

--- deck.go
+++ deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 // Create a new type of 'deck', which is a slice of strings
 type deck []string
@@ -9,4 +13,31 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func generateNewDeck() deck {
+	newDeck := deck{}
+	cardValues := [13]string{"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"}
+	suits := [4]string{"Spades", "Clubs", "Diamonds", "Hearts"}
+	for _, suit := range suits {
+		for _, value := range cardValues {
+			card := value + " of " + suit
+			newDeck = append(newDeck, card)
+		}
+	}
+	return newDeck
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func dealRandom(d deck, handSize int) deck {
+	var cards deck
+	rand.Seed(time.Now().Unix())
+	for i := 0; i < handSize; i++ {
+		card := d[rand.Intn(len(d))]
+		cards = append(cards, card)
+	}
+	return cards
 }
--- main.go
+++ main.go
@@ -2,40 +2,15 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
 func main() {
-	var cards []string
-	fmt.Println(cards)
 	deck := generateNewDeck()
 	deck.print()
-	numberOfCards := 15
-	cards = getNewCards(deck, numberOfCards)
+	numberOfCards := 5
+	hand, remainingCards := deal(deck, numberOfCards)
 	fmt.Println("Your cards are: ")
-	fmt.Println(cards)
-}
-
-func getNewCards(deck []string, numberOfCards int) []string {
-	var cards []string
-	rand.Seed(time.Now().Unix())
-	for i := 0; i < numberOfCards; i++ {
-		card := deck[rand.Intn(len(deck))]
-		cards = append(cards, card)
-	}
-	return cards
-}
-
-func generateNewDeck() deck {
-	var deck []string
-	cardValues := [13]string{"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}
-	suits := [4]string{"Spades", "Clubs", "Diamonds", "Hearts"}
-	for _, face := range cardValues {
-		for _, suit := range suits {
-			card := face + " of " + suit
-			deck = append(deck, card)
-		}
-	}
-	return deck
+	hand.print()
+	fmt.Println("Remaining cards are: ")
+	remainingCards.print()
 }
