3ec90c60aeb1fb2859939fb36556b99ee5870ea0
Leo Rubiano <leofofeo@Leos-MacBook-Pro.local>
2019-07-29 16:52:52 -0500

    final test for saving and loading deck to drive

--- deck_test.go
+++ deck_test.go
@@ -1,10 +1,35 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestGenerateNewDeck(t *testing.T) {
 	d := generateNewDeck()
 	if len(d) != 52 {
 		t.Error("Expected deck length of 52, but got", len(d))
 	}
+
+	if d[0] != "Two of Spades" {
+		t.Error("Expected 'Two of Spades' as first card, but received", d[0])
+	}
+
+	if d[len(d)-1] != "Ace of Hearts" {
+		t.Error("Expected 'Ace of Hearts' as last card, but received", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckTestFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := generateNewDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+	if len(loadedDeck) != 52 {
+		t.Error("Expected 52 cards in deck, received", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
