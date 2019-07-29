bbf6198dda11e548f6e095f72af19406ebe9757d
Leo Rubiano <leofofeo@Leos-MacBook-Pro.local>
2019-07-29 16:40:33 -0500

    create first test for deck length

--- deck_test.go
+++ deck_test.go
@@ -1,1 +1,10 @@
 package main
+
+import "testing"
+
+func TestGenerateNewDeck(t *testing.T) {
+	d := generateNewDeck()
+	if len(d) != 52 {
+		t.Error("Expected deck length of 52, but got", len(d))
+	}
+}
