863bc9e255d5c73a2a884eea5e76b5d1d46e6466
Leo Rubiano <leofofeo@Leos-MacBook-Pro.local>
2019-07-29 16:19:41 -0500

    create basic shuffle function with deck as receiver

--- deck.go
+++ deck.go
@@ -62,3 +62,11 @@ func newDeckFromFile(filename string) deck {
 	s := strings.Split(string(bs), ",")
 	return deck(s)
 }
+
+func (d deck) shuffle() {
+	for i := range d {
+		newPosition := rand.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
+}
